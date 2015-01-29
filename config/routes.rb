ad35e22d61abf1ec99862d0f368c68d44627077c
Mandy Klingbeil <mandy.klingbeil@gmail.com>
2015-01-29 16:48:54 +0100

    change views of groups change locales

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,9 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
+  get 'dashboard' => 'dashboard#dashboard'
   root :to => 'home#index'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
