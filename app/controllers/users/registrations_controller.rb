5da002df339d387456861f0bdd7d317f4458fda4
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2015-02-05 18:17:31 +0100

    add required terms+conditions, first_name and last_name, fix wrong spacing in case of error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,28 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  #before_action :add_resource
+  #after_action :invalidate_resource
 
   def new
      super
   end
 
   def create
-    super
-    @user = User.new(user_params)
-    @user.save()
+    if (user_params.has_key?(:terms_and_conditions_confirmation))
+      super
+      #@user = User.new(user_params)
+      #@user.save()
+    else
+      flash['error'] = t('terms_and_conditions_failure')
+
+      build_resource(sign_up_params)
+      resource.save
+      clean_up_passwords resource
+      #set_minimum_password_length
+      #render :method => 'GET', action: :new
+      session[:resource] = resource
+      redirect_to new_user_registration_path
+    end
   end
 
 
@@ -23,7 +37,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email)
+      params.permit(:terms_and_conditions_confirmation)
+    end
+
+
+    def add_resource
+      @resource = session[:resource]
+    end
+
+    def invalidate_resource
+      session.delete(:resource)
     end
 
 end
