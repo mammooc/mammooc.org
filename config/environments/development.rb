d00614ec85adc586ef70654008104a918c10c568
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-02-09 17:16:20 +0100

    add UserMailer; change smtp setting for ActionMailer in config/enviroments/*.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,18 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # mail configurations
+  # ActionMailer::Base.perform_deliveries = false
+  ActionMailer::Base.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'heroku.com',
+      :authentication => :plain
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
 end
