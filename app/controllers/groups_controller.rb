8054cf9c17e935166c3ae90d8a135738c6b3e9a1
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-02-09 14:03:25 +0100

    fix destroy method for groups; add tests for creating and deleting groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -57,9 +57,7 @@ class GroupsController < ApplicationController
   # DELETE /groups/1
   # DELETE /groups/1.json
   def destroy
-    unless UserGroup.find_by_group_id(@group.id).blank?
-      UserGroup.find_by_group_id(@group.id).destroy
-    end
+    UserGroup.destroy_all(group_id: @group.id)
     @group.destroy
     respond_to do |format|
       format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
