146aa3e8b8d5a733504a91f046579bd34eebf3b7
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-02-12 15:37:32 +0100

    delete comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < ActionController::Base
   def require_login
     unless user_signed_in?
         session[:user_original_url] = request.fullpath
-    #  puts request.original_url
       flash[:error] = "You must be logged in to access this section"
       redirect_to new_user_session_path # halts request cycle
     end
