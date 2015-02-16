23b4b301116f7c990103614ca3cbe329e518170a
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-02-16 17:18:19 +0100

    Add migration in order to allow null values for group_id in group_invitations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216101034) do
+ActiveRecord::Schema.define(version: 20150216151851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,7 +166,7 @@ ActiveRecord::Schema.define(version: 20150216101034) do
   add_index "evaluations", ["user_id"], name: "index_evaluations_on_user_id", using: :btree
 
   create_table "group_invitations", force: :cascade do |t|
-    t.uuid     "group_id",                    null: false
+    t.uuid     "group_id"
     t.string   "token",                       null: false
     t.datetime "expiry_date",                 null: false
     t.datetime "created_at",                  null: false
