b617009122e409fdfd0789b829e62fdcfb78c27c
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-02-23 16:54:26 +0100

    Fix missing setting in factory

--- spec/factories/group_invitations.rb
+++ spec/factories/group_invitations.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :group_invitation do
     association :group_id, factory: :group
-    sequence(:token) { SecureRandom.urlsafe_base64(16)}
+    sequence(:token) { SecureRandom.urlsafe_base64(Settings.token_length)}
     expiry_date Settings.token_expiry_date
     used false
   end
