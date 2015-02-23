3bc4788f505e988f4e25d8abdc68b3834327d498
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-02-23 20:45:43 +0100

    remove  logging for debug purposes

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -13,14 +13,6 @@ class Users::PasswordsController < Devise::PasswordsController
       respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
     else
       redirect_to new_user_password_path
-      #respond_with(resource)
-    end
-    puts ('________________')
-    Rails.logger.debug(resource.errors.inspect)
-    resource.errors.each do |key, value|
-      flash['error'] << "#{t(key)} #{value}"
-      puts ('_GOT AN ERROR <3_')
-      Rails.logger.debug(flash['error'].inspect)
     end
   end
 
