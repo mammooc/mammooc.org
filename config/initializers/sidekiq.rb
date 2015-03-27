14bdbac488a4732d714b3ed9632cbd2e78561e4c
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-03-27 16:33:23 +0100

    Refactor password for redis in sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ require 'sidekiq'
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 
 Sidekiq.configure_server do |config|
-  config.redis = { host: uri.host, port: uri.port, :password => uri.password }
+  config.redis = { host: uri.host, port: uri.port, password: uri.password }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { host: uri.host, port: uri.port, :password => uri.password }
+  config.redis = { host: uri.host, port: uri.port, password: uri.password }
 end
