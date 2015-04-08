d125647bb14e80211a0f96ef85830f9fda461d27
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-04-08 13:33:03 +0200

    Remove CircleCI specific environment variables in order to use default selenium tests without PhantomJS

--- spec/support/wait_for_ajax.rb
+++ spec/support/wait_for_ajax.rb
@@ -7,7 +7,7 @@ module WaitForAjax
       loop until finished_all_ajax_requests?
     end
     # PhantomJS is much faster, a way too fast...
-    if ENV['PHANTOM_JS'] == 'true' || ENV['CIRCLECI'] == 'true'
+    if ENV['PHANTOM_JS'] == 'true'
       sleep(0.33)
     end
   end
