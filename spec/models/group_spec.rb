7463a05fb349e866e5615f783f2f5243ce343cb5
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-04-15 16:42:51 +0200

    Add Group model spec for #250 #151

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Group, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  let(:second_user) { FactoryGirl.create(:user) }
+  let!(:group) { FactoryGirl.create(:group, users: [user, second_user]) }
+  let!(:group_invitation) { FactoryGirl.create(:group_invitation, group: group) }
+  let!(:second_group_invitation) { FactoryGirl.create(:group_invitation, group: group) }
+
+  it "should delete all memberships of a group and it's invitation" do
+    expect(UserGroup.where(user: user, group: group)).to be_present
+    expect(UserGroup.where(user: second_user, group: group)).to be_present
+    expect(GroupInvitation.where(group: group).count).to eq 2
+    expect{group.destroy}.not_to raise_error
+    expect(UserGroup.where(user: user, group: group)).to be_empty
+    expect(UserGroup.where(user: second_user, group: group)).to be_empty
+    expect(GroupInvitation.where(group: group).count).to eq 0
+  end
+
 end
