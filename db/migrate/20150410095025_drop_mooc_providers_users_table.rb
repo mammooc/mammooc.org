0032f0390e9be5326844fc0a64992fcf3672a13b
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-04-16 10:51:55 +0200

    Remove unused and old migration for mooc_provider_users #151

--- db/migrate/20150410095025_drop_mooc_providers_users_table.rb
+++
@@ -1,5 +0,0 @@
-class DropMoocProvidersUsersTable < ActiveRecord::Migration
-  def change
-    drop_table :mooc_providers_users
-  end
-end
--- db/migrate/20150410121942_change_mooc_provider_users_table.rb
+++ db/migrate/20150410121942_change_mooc_provider_users_table.rb
@@ -1,6 +1,6 @@
 class ChangeMoocProviderUsersTable < ActiveRecord::Migration
   def change
-    drop_table :mooc_provider_users
+    drop_table :mooc_providers_users
     create_table :mooc_provider_users, id: :uuid  do |t|
       t.uuid :user_id
       t.references :user, type: 'uuid', index: true
