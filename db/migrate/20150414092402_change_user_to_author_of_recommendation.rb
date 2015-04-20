3030d7b9895e7ab8e9d59a48544e23c6dcac340f
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-04-20 13:50:58 +0200

    #67 add migration user-to-author of recommendations

---
+++ db/migrate/20150414092402_change_user_to_author_of_recommendation.rb
@@ -0,0 +1,9 @@
+class ChangeUserToAuthorOfRecommendation < ActiveRecord::Migration
+  def change
+    remove_reference :recommendations, :user
+
+    add_reference :recommendations, :author, references: :users, index: true, type: 'uuid'
+    add_foreign_key :recommendations, :users, column: :author_id
+
+  end
+end
