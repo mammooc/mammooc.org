b4435c3e0b22f6a971f31b777a4ca394195ea068
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-04-27 16:46:21 +0200

    Add timeout in order to prevent CircleCI from failing when wait_for_ajax didn't work correctly

--- spec/support/wait_for_ajax.rb
+++ spec/support/wait_for_ajax.rb
@@ -3,8 +3,9 @@
 
 module WaitForAjax
   def wait_for_ajax
+    endtime = Time.now + 15.seconds
     Timeout.timeout(Capybara.default_wait_time) do
-      loop until finished_all_ajax_requests?
+      loop until finished_all_ajax_requests? or Time.now > endtime
     end
     # PhantomJS is much faster, a way too fast...
     if ENV['PHANTOM_JS'] == 'true'
