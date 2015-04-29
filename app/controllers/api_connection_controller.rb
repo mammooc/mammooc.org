f021c3213efdd575b0c7599ca314602e79d6fdbf
Sebastian Kliem <sebastian-kliem@live.de>
2015-04-29 16:38:03 +0200

    uncomment other CourseWorkers in ApiConnectionController

--- app/controllers/api_connection_controller.rb
+++ app/controllers/api_connection_controller.rb
@@ -5,10 +5,10 @@ class ApiConnectionController < ApplicationController
   end
 
   def send_request
-    # OpenHPICourseWorker.perform_async
-    # OpenSAPCourseWorker.perform_async
-    # EdxCourseWorker.perform_async
-    # CourseraCourseWorker.perform_async
+    OpenHPICourseWorker.perform_async
+    OpenSAPCourseWorker.perform_async
+    EdxCourseWorker.perform_async
+    CourseraCourseWorker.perform_async
     IversityCourseWorker.perform_async
     redirect_to api_connection_index_path
   end
