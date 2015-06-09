1575c500e92801be81e3c563a1a86af02296d747
Paul Arndt <paul.arndt92@gmail.com>
2015-06-09 15:05:52 +0200

    #106, minor corrections for pr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -203,6 +203,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :primary_email, :title, :password, :profile_image, :about_me) #:email_settings,
+    params.require(:user).permit(:first_name, :last_name, :primary_email, :title, :password, :profile_image, :about_me)
   end
 end
