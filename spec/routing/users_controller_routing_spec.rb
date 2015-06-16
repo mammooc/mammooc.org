81fad01bb1cab23e05874152dea84e1bcf12fe4f
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-06-16 13:02:38 +0200

    #103 delete unnecessary test

--- spec/routing/users_controller_routing_spec.rb
+++ spec/routing/users_controller_routing_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe UsersController, type: :routing do
       expect(get: '/users/1').to route_to('users#show', id: '1')
     end
 
-    it 'routes to #edit' do
-      expect(get: '/users/1/edit').to route_to('users#edit', id: '1')
-    end
-
     it 'routes to #update' do
       expect(put: '/users/1').to route_to('users#update', id: '1')
     end
