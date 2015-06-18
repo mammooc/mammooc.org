4942fc3bbff6715d5c4671b8a88b5d155088b7a1
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-06-18 16:59:40 +0200

    Fix Completions Request Spec #310

--- spec/requests/completions_spec.rb
+++ spec/requests/completions_spec.rb
@@ -2,13 +2,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Completions', type: :request do
+  let(:user) { FactoryGirl.create(:user) }
+
   before(:each) do
     sign_in_as_a_valid_user
   end
 
   describe 'GET /completions' do
     it 'works! (now write some real specs)' do
-      get completions_path
+      get completions_path(user.id)
       expect(response).to have_http_status(200)
     end
   end
