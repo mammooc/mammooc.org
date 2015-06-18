434e03af6ea6a541d3799b6da535de1520988d7b
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-06-18 15:49:08 +0200

    Include new API changes #310

--- app/workers/abstract_xikolo_course_worker.rb
+++ app/workers/abstract_xikolo_course_worker.rb
@@ -17,8 +17,10 @@ class AbstractXikoloCourseWorker < AbstractCourseWorker
     update_map = create_update_map mooc_provider
     course_track_type = CourseTrackType.find_by(type_of_achievement: 'xikolo_record_of_achievement')
 
-    response_data.each do |course_element|
-      next if course_element['isExternal']
+    all_teachers = prepare_teachers_hash(response_data['teachers'])
+
+    response_data['courses'].each do |course_element|
+      next if course_element['isExternal'] || course_element['isHidden'] || course_element['isInviteOnly']
       course = Course.find_by(provider_course_id: course_element['id'], mooc_provider_id: mooc_provider.id)
       if course.nil?
         course = Course.new
@@ -29,20 +31,36 @@ class AbstractXikoloCourseWorker < AbstractCourseWorker
       course.name = course_element['name'].strip
       course.provider_course_id = course_element['id']
       course.mooc_provider_id = mooc_provider.id
-      course.url = course_element['urls']['details'] # self.class::COURSE_LINK_BODY + course_element['course_code']
+      course.url = self.class::COURSE_LINK_BODY + course_element['urls']['details']
       course.language = course_element['language']
-      course.imageId = course_element['image']
-      course.start_date = course_element['startDate']
+      course.imageId = self.class::COURSE_LINK_BODY + course_element['image']
+      course.start_date = course_element['displayStartDate']
       course.end_date = course_element['endDate']
       course.abstract = convert_to_absolute_urls(parse_markdown(course_element['abstract']))
       course.description = convert_to_absolute_urls(parse_markdown(course_element['description']))
-      course.course_instructors = course_element['teachers']
-      course.open_for_registration = !course_element['locked']
+      course.course_instructors = translate_teachers(course_element['teachers'], all_teachers)
+      # course.open_for_registration = !course_element['locked']
       # course.points_maximal = course_element['points_maximal']
       track = CourseTrack.find_by(course_id: course.id, track_type: course_track_type) || CourseTrack.create!(track_type: course_track_type, costs: 0.0, costs_currency: "\xe2\x82\xac")
       course.tracks.push(track)
       course.save!
     end
     evaluate_update_map update_map
+  end
+
+  def prepare_teachers_hash(all_teachers)
+    hash = {}
+    all_teachers.each do |teacher|
+      hash[teacher.id] = teacher.name
+    end
+    hash
+  end
+
+  def translate_teachers(course_teachers, all_teachers)
+    string = ''
+    course_teachers.each do |teacher|
+      string += "#{all_teachers[teacher]}, "
+    end
+    string.present? ? string[0...-2] : string
   end
 end
