bf95e0502408eb71bb03db5ee53686eabf95c41c
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-06-22 11:21:44 +0200

    Move logic from View to controller Fix error on redirection for synchronizing courses from dashboard #310

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -10,6 +10,16 @@ class CompletionsController < ApplicationController
       courses.push(completion.course)
     end
     @provider_logos = AmazonS3.instance.provider_logos_hash_for_courses(courses)
+    @number_of_certificates = []
+    @completions.each do |completion|
+      @number_of_certificates.push completion.certificates.count
+    end
+    puts @number_of_certificates
+    @verify_available = []
+    @completions.each do |completion|
+      @verify_available.push completion.certificates.pluck(:verification_url).reject{|element| element.blank?}.present? ? true : false
+    end
+    puts @verify_available
   end
 
   private
