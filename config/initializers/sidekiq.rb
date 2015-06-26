266da746da984a4c5220906b4564b3f9b746a085
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2015-06-26 11:46:16 +0200

    Prevent Redis & Sidekiq from starting in production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,7 +11,7 @@ Sidekiq.configure_client do |config|
   config.redis = {host: uri.host, port: uri.port, password: uri.password}
 end
 
-if defined?(PhusionPassenger) && ENV['AUTO_START_SIDEKIQ']
+if defined?(PhusionPassenger) && ENV['AUTO_START_SIDEKIQ'] == 'true'
   PhusionPassenger.on_event(:starting_worker_process) do |_forked|
     @sidekiq_pid ||= spawn('bundle exec sidekiq -C ./config/sidekiq.yml')
   end
