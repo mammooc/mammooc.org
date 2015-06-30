59b19e939d9b5c73faf834d8dfd02ffacd1a9e81
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-06-30 11:40:27 +0200

    fix bug deleting bookmark do not delete activity

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,7 +15,9 @@ class BookmarksController < ApplicationController
   end
 
   def delete
-    Bookmark.find_by(user_id: params[:user_id], course_id: params[:course_id]).destroy
+    bookmark = Bookmark.find_by(user_id: params[:user_id], course_id: params[:course_id])
+    bookmark.destroy
+    PublicActivity::Activity.find_by(trackable_id: bookmark.id, trackable_type: 'Bookmark' , owner: params[:user_id], owner_type: 'User').destroy
     redirect_to bookmarks_path
   end
 
