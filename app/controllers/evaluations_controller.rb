905fb856b78a55115bab29b748309632b3d18e4b
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-07-21 11:54:41 +0200

    #456 refactor method process_evaluation_rating
    rename to process_feedback

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,10 +1,10 @@
 # -*- encoding : utf-8 -*-
 class EvaluationsController < ApplicationController
-  before_action :set_evaluation, only: [:process_evaluation_rating]
+  before_action :set_evaluation, only: [:process_feedback]
 
   respond_to :html
 
-  def process_evaluation_rating
+  def process_feedback
     unless @evaluation.user == current_user
       if params['helpful'] == 'true'
         @evaluation.positive_feedback_count += 1
@@ -18,7 +18,7 @@ class EvaluationsController < ApplicationController
     respond_to do |format|
       begin
         format.html { redirect_to dashboard_path }
-        format.json { render :process_evaluation_rating_result, status: :ok }
+        format.json { render :process_feedback_result, status: :ok }
       rescue StandardError => e
         format.html { redirect_to dashboard_path }
         format.json { render json: e.to_json, status: :unprocessable_entity }
--- spec/controllers/evaluations_controller_spec.rb
+++ spec/controllers/evaluations_controller_spec.rb
@@ -26,14 +26,14 @@ RSpec.describe EvaluationsController, type: :controller do
     sign_in user
   end
 
-  describe 'POST process_evaluation_rating' do
+  describe 'POST process_feedback' do
     let(:evaluation) { FactoryGirl.create(:full_evaluation) }
     let(:own_evaluation) { FactoryGirl.create(:full_evaluation, user_id: user.id) }
 
     it 'increases rating_count by one when evaluation is marked as not helpful' do
       total_feedback_count = evaluation.total_feedback_count
       positive_feedback_count = evaluation.positive_feedback_count
-      post :process_evaluation_rating, id: evaluation.id, helpful: 'false'
+      post :process_feedback, id: evaluation.id, helpful: 'false'
       evaluation.reload
       expect(evaluation.total_feedback_count).to eq(total_feedback_count + 1)
       expect(evaluation.positive_feedback_count).to eq(positive_feedback_count)
@@ -42,7 +42,7 @@ RSpec.describe EvaluationsController, type: :controller do
     it 'increases rating_count and helpful_rating_count by one when evaluation is marked as helpful' do
       total_feedback_count = evaluation.total_feedback_count
       positive_feedback_count = evaluation.positive_feedback_count
-      post :process_evaluation_rating, id: evaluation.id, helpful: 'true'
+      post :process_feedback, id: evaluation.id, helpful: 'true'
       evaluation.reload
       expect(evaluation.total_feedback_count).to eq(total_feedback_count + 1)
       expect(evaluation.positive_feedback_count).to eq(positive_feedback_count + 1)
@@ -51,7 +51,7 @@ RSpec.describe EvaluationsController, type: :controller do
     it 'does not increase anything when rated an own evaluation' do
       total_feedback_count = own_evaluation.total_feedback_count
       positive_feedback_count = own_evaluation.positive_feedback_count
-      post :process_evaluation_rating, id: own_evaluation.id, helpful: 'true'
+      post :process_feedback, id: own_evaluation.id, helpful: 'true'
       own_evaluation.reload
       expect(own_evaluation.total_feedback_count).not_to eq(total_feedback_count + 1)
       expect(own_evaluation.positive_feedback_count).not_to eq(positive_feedback_count + 1)
