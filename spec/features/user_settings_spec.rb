077621f2fd48fdfac5a2101a200b238fc4486401
rosa.braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-07-22 16:22:16 +0200

    #444 rake rubocop:run

--- spec/features/user_settings_spec.rb
+++ spec/features/user_settings_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'UserSettings', type: :feature do
 
   describe 'course enrollments' do
     let(:course1) { FactoryGirl.create(:course) }
-    let(:second_user) { FactoryGirl.create(:user, courses:[course1]) }
+    let(:second_user) { FactoryGirl.create(:user, courses: [course1]) }
     let(:third_user) { FactoryGirl.create(:user) }
     let(:fourth_user) { FactoryGirl.create(:user) }
     let(:fifth_user) { FactoryGirl.create(:user) }
@@ -32,7 +32,6 @@ RSpec.describe 'UserSettings', type: :feature do
     let!(:user_setting_entry) { FactoryGirl.create(:user_setting_entry, setting: user_setting, key: 'users', value: [user.id]) }
     let(:user_setting4) { FactoryGirl.create(:user_setting, name: :course_enrollments_visibility, user: second_user) }
     let!(:user_setting_entry4) { FactoryGirl.create(:user_setting_entry, setting: user_setting4, key: 'groups', value: [group.id]) }
-
 
     context "on user's profile" do
       let(:user_setting2) { FactoryGirl.create(:user_setting, name: :profile_visibility, user: second_user) }
@@ -79,11 +78,10 @@ RSpec.describe 'UserSettings', type: :feature do
         expect(page).not_to have_content I18n.t('users.profile.current_courses')
         expect(page).not_to have_content course1.name
       end
-
     end
 
     context 'in newsfeed' do
-      let!(:activity) { FactoryGirl.create(:activity_course_enroll, owner: second_user, trackable: course1, user_ids: [user.id, third_user.id, fourth_user.id, fifth_user.id], group_ids: [group.id, second_group.id] ) }
+      let!(:activity) { FactoryGirl.create(:activity_course_enroll, owner: second_user, trackable: course1, user_ids: [user.id, third_user.id, fourth_user.id, fifth_user.id], group_ids: [group.id, second_group.id]) }
 
       it 'is visible for users who are whitelisted' do
         capybara_sign_in user
@@ -132,7 +130,7 @@ RSpec.describe 'UserSettings', type: :feature do
   describe 'course results' do
     let(:course1) { FactoryGirl.create(:course) }
     let!(:course1_completions) { FactoryGirl.create(:full_completion, user: second_user, course: course1) }
-    let(:second_user) { FactoryGirl.create(:user, courses:[course1]) }
+    let(:second_user) { FactoryGirl.create(:user, courses: [course1]) }
     let(:third_user) { FactoryGirl.create(:user) }
     let(:fourth_user) { FactoryGirl.create(:user) }
     let(:fifth_user) { FactoryGirl.create(:user) }
@@ -197,7 +195,6 @@ RSpec.describe 'UserSettings', type: :feature do
     end
 
     context 'on completions page' do
-
       it 'page is accessable for user himself' do
         capybara_sign_in second_user
         visit completions_path(second_user)
