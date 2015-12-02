38bb14cf67eea61ae96ff419ddeb2fe30e5a3963
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-12-02 16:08:40 +0100

    add routing tests for user_dates

--- spec/routing/user_dates_routing_spec.rb
+++ spec/routing/user_dates_routing_spec.rb
@@ -1,10 +1,30 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserDatesController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/user_dates").to route_to("user_dates#index")
+    it 'routes to #index' do
+      expect(get: '/user_dates').to route_to('user_dates#index')
+    end
+
+    it 'routes to #synchronize_dates_on_dashboard' do
+      expect(get: 'user_dates/synchronize_dates_on_dashboard').to route_to('user_dates#synchronize_dates_on_dashboard')
+    end
+
+    it 'routes to #synchronize_dates_on_index_page' do
+      expect(get: 'user_dates/synchronize_dates_on_index_page').to route_to('user_dates#synchronize_dates_on_index_page')
+    end
+
+    it 'routes to #calendar_feed' do
+      expect(get: 'user_dates/calendar_feed').to route_to('user_dates#create_calendar_feed')
+    end
+
+    it 'route to #get_my_dates' do
+      expect(get: 'user_dates/get_my_dates/:token').to  route_to('user_dates#get_my_dates')
+    end
+
+    it 'routes to #events_for_calendar_view' do
+      expect(get: 'user_dates/events_for_calendar_view').to route_to('user_dates#events_for_calendar_view')
     end
 
   end
