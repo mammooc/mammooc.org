423b590ce16f812b7dbb6176b3cbe8904757c9c6
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-12-02 16:39:54 +0100

    add request tests for get_my_dates (user_dates)
    add possibility to login in request tests (change rails helper)

--- spec/requests/user_dates_spec.rb
+++ spec/requests/user_dates_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "UserDates", type: :request do
-  describe "GET /user_dates" do
-    it "works! (now write some real specs)" do
-      get user_dates_path
+RSpec.describe 'UserDates', type: :request do
+  describe 'get_my_dates' do
+    let!(:user) { FactoryGirl.create(:user, token_for_user_dates: '1234') }
+
+    it 'is possible to get get_my_dates without login with valid token' do
+      get '/user_dates/get_my_dates/1234', format: :ics
       expect(response).to have_http_status(200)
     end
+
+    it 'returns status 404 when getting get_my_dates without valid token' do
+      get '/user_dates/get_my_dates/4321', format: :ics
+      expect(response).to have_http_status(404)
+    end
+
+    it 'is possible to get get_my_dates when logged in with valid token' do
+      login_as(user, scope: :user)
+      get '/user_dates/get_my_dates/1234', format: :ics
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns status 404 when getting get_my_dates when logged in without valid token' do
+      login_as(user, scope: :user)
+      get '/user_dates/get_my_dates/4321', format: :ics
+      expect(response).to have_http_status(404)
+    end
+
   end
 end
