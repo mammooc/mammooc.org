88f8b8785f88288bac075d31e7c6e09c57d609b6
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2016-01-30 18:03:44 +0100

    #577 add other xikolo platforms to date synchronization

--- app/controllers/concerns/cnmooc_house_connector.rb
+++ app/controllers/concerns/cnmooc_house_connector.rb
@@ -2,5 +2,6 @@
 class CnmoocHouseConnector < AbstractXikoloConnector
   NAME = 'cnmooc.house'
   ROOT_API = 'https://cnmooc.house/api/'
+  ROOT_API_V2 = 'https://cnmooc.house/api/v2/'
   COURSE_LINK = 'https://cnmooc.house/courses/'
 end
--- app/workers/user_dates_worker.rb
+++ app/workers/user_dates_worker.rb
@@ -9,5 +9,10 @@ class UserDatesWorker
   def synchronize_dates_for_all_users
     OpenHPIConnector.new.load_dates_for_users
     OpenSAPConnector.new.load_dates_for_users
+    MoocHouseConnector.new.load_dates_for_users
+    CnmoocHouseConnector.new.load_dates_for_users
+    OpenHPIChinaConnector.new.load_dates_for_users
+    OpenSAPChinaConnector.new.load_dates_for_users
+    OpenUNEConnector.new.load_dates_for_users
   end
 end
