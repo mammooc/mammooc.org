669494ecd6f7245b5631e0679654118d6af07d5f
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2016-01-31 20:21:44 +0100

    #577 modify tests for other xikolo platforms

--- spec/workers/user_dates_worker_spec.rb
+++ spec/workers/user_dates_worker_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe UserDatesWorker do
   it 'loads dates for openHPI and openSAP' do
     expect_any_instance_of(OpenHPIConnector).to receive(:load_dates_for_users)
     expect_any_instance_of(OpenSAPConnector).to receive(:load_dates_for_users)
+    expect_any_instance_of(MoocHouseConnector).to receive(:load_dates_for_users)
+    expect_any_instance_of(CnmoocHouseConnector).to receive(:load_dates_for_users)
+    expect_any_instance_of(OpenHPIChinaConnector).to receive(:load_dates_for_users)
+    expect_any_instance_of(OpenSAPChinaConnector).to receive(:load_dates_for_users)
+    expect_any_instance_of(OpenUNEConnector).to receive(:load_dates_for_users)
     described_class.perform_async
   end
 end
