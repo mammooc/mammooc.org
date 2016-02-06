e62deac9eba8a5d67973bcac9644cad9bfb79f77
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-02-06 15:12:01 +1100

    Run rubocop

--- db/migrate/20160206035823_disallow_null_values_for_timestamps_in_activities.rb
+++ db/migrate/20160206035823_disallow_null_values_for_timestamps_in_activities.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
 class DisallowNullValuesForTimestampsInActivities < ActiveRecord::Migration
   def change
     change_column_null :activities, :created_at, false
