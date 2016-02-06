9fb004b73118f81c359052b700fb12515fd80a00
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-02-06 15:50:00 +1100

    Set selenium window size to the same as poltergeist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,7 +55,9 @@ else
     profile = Selenium::WebDriver::Firefox::Profile.new
     profile['intl.accept_languages'] =  'en'
     capabilities = Selenium::WebDriver::Remote::Capabilities.firefox(elementScrollBehavior: 1)
-    Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, desired_capabilities: capabilities)
+    driver = Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, desired_capabilities: capabilities)
+    driver.browser.manage.window.resize_to(1280,960)
+    driver
   end
   Capybara.javascript_driver = :selenium
 end
