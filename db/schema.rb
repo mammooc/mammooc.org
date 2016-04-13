03db7addac9fc128195bdf4ef6cf4060a7079dd5
Kirstin Heidler <kirstin.heidler@student.hpi.uni-potsdam.de>
2016-04-13 14:12:18 +0200

    schema changed in response to migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301172138) do
+ActiveRecord::Schema.define(version: 20160406145634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -315,6 +315,7 @@ ActiveRecord::Schema.define(version: 20160301172138) do
     t.integer  "profile_image_file_size"
     t.datetime "profile_image_updated_at"
     t.string   "token_for_user_dates"
+    t.boolean  "no_email",                   default: false
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
