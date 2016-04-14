f61d0010af7ecc4ba8400c589943fb0a039ce096
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-04-14 15:38:05 +0200

    Dynamically calculate the group invitation expiry date

--- spec/factories/group_invitations.rb
+++ spec/factories/group_invitations.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
   factory :group_invitation do
     association :group_id, factory: :group
     sequence(:token) { SecureRandom.urlsafe_base64(Settings.token_length) }
-    expiry_date Settings.token_expiry_date
+    expiry_date { Time.zone.now + Settings.token_expiry_duration }
     used false
   end
 end
