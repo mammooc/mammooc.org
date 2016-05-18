9b21a6405ebead7c4bd8bb86cbce935308d1daa1
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-05-18 20:49:03 +0200

    Refactor finish signup process Fix Language-related bug when updating email addresses

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
     patch '/users' => 'users/registrations#update'
     put '/users' => 'users/registrations#update'
     delete '/users' => 'users/registrations#destroy'
-    match '/users/finish_signup' => 'users/registrations#finish_signup', via: [:get, :patch], :as => :finish_signup
+    get '/users/finish_signup' => 'users/registrations#finish_signup', :as => :finish_signup
     match '/users/auth/easyID' => 'users/omniauth_callbacks#easy_id', via: [:get, :post], :as => :easy_id
     get '/users/deauth/:provider' => 'users/omniauth_callbacks#deauthorize', as: :omniauth_deauthorize
   end
