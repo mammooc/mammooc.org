200f75d153e1ceb2e1afe9680fb33d2420c2e264
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-06-10 14:53:22 +0200

    Update Xikolo OAuth Strategy to work with integrated OAuth service

--- lib/omni_auth/strategies/xikolo.rb
+++ lib/omni_auth/strategies/xikolo.rb
@@ -6,20 +6,25 @@ module OmniAuth
       option :name, :xikolo
 
       option :client_options, {
-          :site => "http://localhost:3001",
-          :authorize_url => "/oauth/authorize"
+          site: 'http://localhost:3001',
+          authorize_url: '/oauth/authorize'
       }
 
-      uid { raw_info["id"] }
+      uid { raw_info['id'] }
 
       info do
         {
-            :email => raw_info["email"]
+            email: raw_info['email'],
+            first_name: raw_info['first_name'],
+            last_name: raw_info['last_name'],
+            image: 'http://localhost:3000' + '/avatar/' + raw_info['id']
         }
       end
 
       def raw_info
         @raw_info ||= access_token.get('/api/user').parsed
+        puts @raw_info
+        @raw_info
       end
     end
   end
