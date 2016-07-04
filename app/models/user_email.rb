65fd93ded818d4ffad188f4dfab1b9ca2c55e8e6
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-07-04 23:37:57 +0200

    Fix bug when applying seed data

--- app/models/user_email.rb
+++ app/models/user_email.rb
@@ -45,15 +45,16 @@ class UserEmail < ActiveRecord::Base
 
   def one_primary_address_per_user
     if is_primary
-      if UserEmail.where(user_id: user, is_primary: true).size == 1 && UserEmail.find_by(user_id: user, is_primary: true).id != id
+      primary_address = UserEmail.where(user: user, is_primary: true)
+      if primary_address.size == 1 && primary_address.first.id != id
         # One primary address in DB, adding another one
         errors.add(:is_primary, 'could not be set because another primary address is already stored')
       end
     elsif !is_primary
-      if UserEmail.where(user_id: user, is_primary: true).empty?
+      if primary_address.empty?
         # No primary address in DB
         errors.add(:is_primary, 'must add this address as primary email')
-      elsif UserEmail.where(user_id: user, is_primary: true).size == 1 && UserEmail.find_by(user_id: user, is_primary: true).id == id
+      elsif primary_address.size == 1 && primary_address.first.id == id
         # Update last existing primary address
         errors.add(:is_primary, 'could not be changed because there is no other primary address')
       end
@@ -62,7 +63,7 @@ class UserEmail < ActiveRecord::Base
 
   def validate_destroy
     return unless is_primary
-    return if UserEmail.where(user_id: user, is_primary: true).size == 1
+    return if UserEmail.where(user: user, is_primary: true).size == 1
     return if User.where(id: user).blank?
     UserEmail.new(attributes.except('created_at', 'updated_at')).save!
     raise ActiveRecord::RecordNotDestroyed('There must be exactly one primary address for a user')
