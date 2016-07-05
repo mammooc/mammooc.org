3da2d7a662649c6350a9778a9dca8a1fc18115a5
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-07-05 11:18:00 +0200

    Change Devise::TestHelpers to use renamed Devise::Test::ControllerHelpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,8 +34,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers, type: :request
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
