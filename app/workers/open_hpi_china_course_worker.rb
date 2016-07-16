eb3f4c1c3d9287d3ba97660ea1a6ff0fe1b8872e
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-07-16 17:16:00 +0200

    Change mooc provider names in workers to match the names in our seeds

--- app/workers/open_hpi_china_course_worker.rb
+++ app/workers/open_hpi_china_course_worker.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OpenHPIChinaCourseWorker < AbstractXikoloCourseWorker
-  MOOC_PROVIDER_NAME = 'openHPI China'
+  MOOC_PROVIDER_NAME = 'openHPI.cn'
   MOOC_PROVIDER_API_LINK = 'https://openhpi.cn/api/courses'
   COURSE_LINK_BODY = 'https://openhpi.cn/courses/'
 end
