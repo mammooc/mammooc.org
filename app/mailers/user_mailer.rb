b3788d72427b42f3c0a6593d12cde790a2650383
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2016-07-26 15:48:09 +0200

    add number of new courses to newsletter subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -23,8 +23,9 @@ class UserMailer < ApplicationMailer
     unless @courses.blank?
       @provider_logos = AmazonS3.instance.provider_logos_hash_for_courses(@courses)
     end
+    number_of_new_courses = @courses.length
 
-    mail(to: email_adress, subject: t('newsletter.email.subject'))
+    mail(to: email_adress, subject: t('newsletter.email.subject', number: number_of_new_courses))
   end
 
   def obligatory_recommendation_user_notification(email_adress, user, course, current_user, root_url)
