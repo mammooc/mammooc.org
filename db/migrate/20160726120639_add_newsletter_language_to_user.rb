deb72594e557fe84e4d9f2baa2eaefff5b52e262
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2016-08-04 15:18:13 +0200

    change default to english

--- db/migrate/20160726120639_add_newsletter_language_to_user.rb
+++ db/migrate/20160726120639_add_newsletter_language_to_user.rb
@@ -1,5 +1,5 @@
 class AddNewsletterLanguageToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :newsletter_language, :string, default: nil
+    add_column :users, :newsletter_language, :string, default: 'en'
   end
 end
