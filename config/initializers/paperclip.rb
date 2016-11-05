27bdc63fa71b6bebb07c4d412531ffde2d1edf2d
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-11-05 13:47:59 +0100

    Fix S3 for course images

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,5 +3,6 @@
 Paperclip::Attachment.default_options[:url] = ':s3_path_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_region] = ENV['AWS_REGION']
+Paperclip::Attachment.default_options[:s3_host_name] = "s3.dualstack.#{ENV['AWS_REGION']}.amazonaws.com"
 Paperclip::Attachment.default_options[:s3_protocol] = :https
 Paperclip::Attachment.default_options[:s3_url_options] = {use_dualstack_endpoint: true}
