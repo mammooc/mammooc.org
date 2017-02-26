206abd3e8797e79c19a05c8fcbcd654e58a52f76
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-02-26 17:20:03 +0100

    Revert JSON API Gem inclusion in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include JSONAPI::ActsAsResourceController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   before_action :require_login, :set_language, :user_picture, :ensure_signup_complete
 
