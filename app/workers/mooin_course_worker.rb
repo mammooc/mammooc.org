e594930f16b132ff037dab2d306675e78178166e
MrSerth <MrSerth@users.noreply.github.com>
2017-05-04 12:59:52 +0200

    Fix JSON API worker (#739)
    * Fix mooin courser worker if course URL is not provided
    
    * Fix spec for new URL

--- app/workers/mooin_course_worker.rb
+++ app/workers/mooin_course_worker.rb
@@ -3,4 +3,5 @@
 class MooinCourseWorker < AbstractJsonApiCourseWorker
   MOOC_PROVIDER_NAME = 'mooin'
   MOOC_PROVIDER_API_LINK = 'https://moodalis.oncampus.de/files/moochub.php'
+  COURSE_LINK_BODY = 'https://mooin.oncampus.de/local/ildcourseinfo/index.php?id='
 end
