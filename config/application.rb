3eb4f0b6972ba4dd77714100379effb8a8f5dcd9
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-05-30 10:20:49 +0200

    Downgrade to Rails 5.0

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module MAMMOOC
     GC::Profiler.enable
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
