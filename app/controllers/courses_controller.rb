53e5ec13e4ade1f546be7a7816a0c53a968656cb
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-07-15 21:32:48 +0200

    Fix empty filterrific_params

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -231,7 +231,13 @@ class CoursesController < ApplicationController
   end
 
   def load_courses
-    @filterrific = initialize_filterrific(Course, params[:filterrific].permit!.to_h,
+    if params[:filterrific]
+      filterrific_params = params[:filterrific].permit!.to_h
+    else
+      filterrific_params = nil
+    end
+
+    @filterrific = initialize_filterrific(Course, filterrific_params,
       select_options: {with_language: Course.options_for_languages,
                        with_mooc_provider_id: MoocProvider.options_for_select,
                        with_subtitle_languages: Course.options_for_subtitle_languages,
