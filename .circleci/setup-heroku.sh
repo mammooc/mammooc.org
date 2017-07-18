027089959bbe474b8303c8c0dcb87b3383727dc5
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-07-18 10:15:44 +0200

    Add heroku to knwon hosts file

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -13,3 +13,7 @@ machine git.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
 EOF
+
+# Add heroku.com to the list of known hosts
+touch ~/.ssh/known_hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
