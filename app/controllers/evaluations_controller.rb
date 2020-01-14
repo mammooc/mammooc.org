67463da033d7b140e0760637d2706b8c2cdb207d
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-01-14 15:37:50 +0100

    Update 404 symbol to use :not_found instead of :recordNotFound

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -91,7 +91,7 @@ class EvaluationsController < ApplicationController
     end
   rescue ActionController::ParameterMissing, ActiveRecord::RecordNotFound => e
     respond_to do |format|
-      format.json { render json: {error: e.message}, status: :recordNotFound }
+      format.json { render json: {error: e.message}, status: :not_found }
     end
   end
 
