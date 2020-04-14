2a241f38ac1e292db7c392b9ba69673289f3ece4
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-04-14 11:17:07 +0200

    Update path for all log entries

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,26 +27,26 @@ set :output, Whenever.path + '/log/whenever/whenever_$(date +%Y%m%d%H%M%S).log'
 set :job_template, "set -o allexport; source #{Whenever.path}/config/.env; set +o allexport; sh -c ':job'"
 
 every 1.day, at: '12:00 am' do
-  command 'echo Cronjob every day at 12:00 am. Executed: `date`', output: '/var/log/cron_check.log'
+  command 'echo Cronjob every day at 12:00 am. Executed: `date`', output: Whenever.path + '/log/whenever/whenever_$(date +%Y%m%d%H%M%S).log'
   rake 'mammooc:update_course_data'
 end
 
 # Waiting for one hour to finish the course workers
 
 every 1.day, at: '1:00 am' do
-  command 'echo Cronjob every day at 01:00 am. Executed: `date`', output: '/var/log/cron_check.log'
+  command 'echo Cronjob every day at 01:00 am. Executed: `date`', output: Whenever.path + '/log/whenever/whenever_$(date +%Y%m%d%H%M%S).log'
   rake 'mammooc:update_user_data'
 end
 
 # Send newsletters at 8 am UTC
 
 every 1.day, at: '8:00 am' do
-  command 'echo Cronjob every day at 08:00 am. Executed: `date`', output: '/var/log/cron_check.log'
+  command 'echo Cronjob every day at 08:00 am. Executed: `date`', output: Whenever.path + '/log/whenever/whenever_$(date +%Y%m%d%H%M%S).log'
   rake 'mammooc:send_reminders'
   rake 'mammooc:send_newsletters'
 end
 
 every 1.hour do
-  command 'echo Cronjob every hour. Executed: `date`', output: '/var/log/cron_check.log'
+  command 'echo Cronjob every hour. Executed: `date`', output: Whenever.path + '/log/whenever/whenever_$(date +%Y%m%d%H%M%S).log'
   rake 'mammooc:synchronize_dates_for_all_users'
 end
