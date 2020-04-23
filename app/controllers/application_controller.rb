1451786f7c85e0189551a3a19f7366d75a97bf3d
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-04-23 14:10:09 +0200

    Set raven context for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_login, :set_language, :user_picture, :ensure_signup_complete
+  before_action :set_raven_context, :require_login, :set_language, :user_picture, :ensure_signup_complete
 
   def user_picture
     return unless current_user
 
     @thumbnail_picture = @current_user.profile_image.expiring_url(3600, :thumb)
+  end
+
+  def set_raven_context
+    return if current_user.blank?
+
+    Raven.user_context(id: current_user.id, email: current_user.primary_email, name: current_user.full_name)
   end
 
   def after_sign_in_path_for(resource)
