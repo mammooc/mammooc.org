d5648a746e7807269a8b43258c9b2430c964355a
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-12-10 16:33:15 +0100

    Update i18n generated JS file

--- public/javascripts/i18n.js
+++ public/javascripts/i18n.js
@@ -402,7 +402,7 @@
 
     while (locales.length) {
       locale = locales.shift();
-      scopes = fullScope.split(this.defaultSeparator);
+      scopes = fullScope.split(options.separator || this.defaultSeparator);
       translations = this.translations[locale];
 
       if (!translations) {
@@ -459,7 +459,7 @@
 
     while (locales.length) {
       locale = locales.shift();
-      scopes = scope.split(this.defaultSeparator);
+      scopes = scope.split(options.separator || this.defaultSeparator);
       translations = this.translations[locale];
 
       if (!translations) {
@@ -679,13 +679,13 @@
       var s = scope.split('.').slice(-1)[0];
       //replace underscore with space && camelcase with space and lowercase letter
       return (this.missingTranslationPrefix.length > 0 ? this.missingTranslationPrefix : '') +
-          s.replace('_',' ').replace(/([a-z])([A-Z])/g,
+          s.replace(/_/g,' ').replace(/([a-z])([A-Z])/g,
           function(match, p1, p2) {return p1 + ' ' + p2.toLowerCase()} );
     }
 
     var localeForTranslation = (options != null && options.locale != null) ? options.locale : this.currentLocale();
     var fullScope           = this.getFullScope(scope, options);
-    var fullScopeWithLocale = [localeForTranslation, fullScope].join(this.defaultSeparator);
+    var fullScopeWithLocale = [localeForTranslation, fullScope].join(options.separator || this.defaultSeparator);
 
     return '[missing "' + fullScopeWithLocale + '" translation]';
   };
@@ -779,8 +779,8 @@
   I18n.toCurrency = function(number, options) {
     options = this.prepareOptions(
         options
-      , this.lookup("number.currency.format")
-      , this.lookup("number.format")
+      , this.lookup("number.currency.format", options)
+      , this.lookup("number.format", options)
       , CURRENCY_FORMAT
     );
 
@@ -799,17 +799,17 @@
 
     switch (scope) {
       case "currency":
-        return this.toCurrency(value);
+        return this.toCurrency(value, options);
       case "number":
-        scope = this.lookup("number.format");
+        scope = this.lookup("number.format", options);
         return this.toNumber(value, scope);
       case "percentage":
-        return this.toPercentage(value);
+        return this.toPercentage(value, options);
       default:
         var localizedValue;
 
         if (scope.match(/^(date|time)/)) {
-          localizedValue = this.toTime(scope, value);
+          localizedValue = this.toTime(scope, value, options);
         } else {
           localizedValue = value.toString();
         }
@@ -897,7 +897,7 @@
   //     %d     - Day of the month (01..31)
   //     %-d    - Day of the month (1..31)
   //     %H     - Hour of the day, 24-hour clock (00..23)
-  //     %-H    - Hour of the day, 24-hour clock (0..23)
+  //     %-H/%k - Hour of the day, 24-hour clock (0..23)
   //     %I     - Hour of the day, 12-hour clock (01..12)
   //     %-I/%l - Hour of the day, 12-hour clock (1..12)
   //     %m     - Month of the year (01..12)
@@ -914,8 +914,8 @@
   //     %Y     - Year with century
   //     %z/%Z  - Timezone offset (+0545)
   //
-  I18n.strftime = function(date, format) {
-    var options = this.lookup("date")
+  I18n.strftime = function(date, format, options) {
+    var options = this.lookup("date", options)
       , meridianOptions = I18n.meridian()
     ;
 
@@ -961,6 +961,7 @@
     format = format.replace("%-d", day);
     format = format.replace("%H", padding(hour));
     format = format.replace("%-H", hour);
+    format = format.replace("%k", hour);
     format = format.replace("%I", padding(hour12));
     format = format.replace("%-I", hour12);
     format = format.replace("%l", hour12);
@@ -983,9 +984,9 @@
   };
 
   // Convert the given dateString into a formatted date.
-  I18n.toTime = function(scope, dateString) {
+  I18n.toTime = function(scope, dateString, options) {
     var date = this.parseDate(dateString)
-      , format = this.lookup(scope)
+      , format = this.lookup(scope, options)
     ;
 
     // A date input of `null` or `undefined` will be returned as-is
@@ -1002,15 +1003,15 @@
       return date_string;
     }
 
-    return this.strftime(date, format);
+    return this.strftime(date, format, options);
   };
 
   // Convert a number into a formatted percentage value.
   I18n.toPercentage = function(number, options) {
     options = this.prepareOptions(
         options
-      , this.lookup("number.percentage.format")
-      , this.lookup("number.format")
+      , this.lookup("number.percentage.format", options)
+      , this.lookup("number.format", options)
       , PERCENTAGE_FORMAT
     );
 
@@ -1024,6 +1025,7 @@
       , iterations = 0
       , unit
       , precision
+      , fullScope
     ;
 
     while (size >= kb && iterations < 4) {
@@ -1032,10 +1034,12 @@
     }
 
     if (iterations === 0) {
-      unit = this.t("number.human.storage_units.units.byte", {count: size});
+      fullScope = this.getFullScope("number.human.storage_units.units.byte", options);
+      unit = this.t(fullScope, {count: size});
       precision = 0;
     } else {
-      unit = this.t("number.human.storage_units.units." + SIZE_UNITS[iterations]);
+      fullScope = this.getFullScope("number.human.storage_units.units." + SIZE_UNITS[iterations], options);
+      unit = this.t(fullScope);
       precision = (size - Math.floor(size) === 0) ? 0 : 1;
     }
 
@@ -1052,7 +1056,7 @@
 
     // Deal with the scope as an array.
     if (isArray(scope)) {
-      scope = scope.join(this.defaultSeparator);
+      scope = scope.join(options.separator || this.defaultSeparator);
     }
 
     // Deal with the scope option provided through the second argument.
@@ -1060,7 +1064,7 @@
     //    I18n.t('hello', {scope: 'greetings'});
     //
     if (options.scope) {
-      scope = [options.scope, scope].join(this.defaultSeparator);
+      scope = [options.scope, scope].join(options.separator || this.defaultSeparator);
     }
 
     return scope;
@@ -1083,9 +1087,9 @@
   };
 
   // Set aliases, so we can save some typing.
-  I18n.t = I18n.translate;
-  I18n.l = I18n.localize;
-  I18n.p = I18n.pluralize;
+  I18n.t = I18n.translate.bind(I18n);
+  I18n.l = I18n.localize.bind(I18n);
+  I18n.p = I18n.pluralize.bind(I18n);
 
   return I18n;
 }));
