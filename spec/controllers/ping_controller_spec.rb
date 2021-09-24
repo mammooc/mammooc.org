a84b279f375fee7413ed32dab93994d4676dcbe0
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-09-24 09:40:51 +0200

    Mock redis_connected! for RSpec

--- spec/controllers/ping_controller_spec.rb
+++ spec/controllers/ping_controller_spec.rb
@@ -4,6 +4,14 @@ require 'rails_helper'
 
 RSpec.describe PingController, type: :controller do
   describe 'index' do
+
+    let(:controller) { instance_double(described_class) }
+
+    before do
+      allow(described_class).to receive(:new).and_return(controller)
+      allow(controller).to receive(:redis_connected!).and_return(true)
+    end
+
     it 'returns the wanted page and answer with HTTP Status 200' do
       get :index
 
